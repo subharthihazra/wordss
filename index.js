59f24476aa3b83e76b1395f216f0c73423d735cf
subharthi75 <subharthi75@gmail.com>
2023-12-21 01:47:49 +0530

    scaped data

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-axios({
-  method: "GET",
-  url: baseUrl,
-  headers: {
-    "User-Agent": rua,
-  },
-})
-  .then(function (response) {
-    const $ = cheerio.load(response.data);
-    if (wordOfDay.length > 0) {
-      wordOfDay = [];
-    }
+import randomUseragent from "random-useragent";
+import fs from "fs";
+const rua = randomUseragent.getRandom();
 
-    var post = $(".section #shared_section");
-    var word = post
-      .find("#random_word")
-      .eq(0)
-      .text()
-      .replace("\r\n\t\t\t\t\t", "")
-      .replace("\r\n\t\t\t\t", "")
-      .replace("\n\t\t\t\t\t", "")
-      .replace("\n\t\t\t\t", "");
-    var definition = post
-      .find("#random_word_definition")
-      .eq(0)
-      .text()
-      .replace("\n", "");
-    var pronounceword = pronounce(word).replace(",", "");
+const iter = 1000;
+const baseUrl = "https://randomword.com";
+const partOfSpeech = "adjective";
+const filePath = `data/${partOfSpeech}.txt`;
 
-    wordOfDay.push({
-      word: decodeURI(word.charAt(0).toUpperCase() + word.slice(1)),
-      definition: decodeURI(
-        definition.charAt(0).toUpperCase() + definition.slice(1)
-      ),
-      pronunciation: decodeURI(
-        pronounceword.charAt(0).toUpperCase() + pronounceword.slice(1)
-      ),
-    });
-    res.send(JSON.stringify(wordOfDay, null, 2));
+for (let i = 0; i < iter; i++) {
+  axios({
+    method: "GET",
+    url: `${baseUrl}/${partOfSpeech}`,
+    headers: {
+      "User-Agent": rua,
+    },
   })
-  .catch(function (error) {
-    if (!error.response) {
-      console.log("API URL is Missing");
-      res.json("API URL is Missing");
-    } else {
-      console.log("Something Went Wrong - Enter the Correct API URL");
-      res.json("Something Went Wrong - Enter the Correct API URL");
-    }
-  });
+    .then(function (response) {
+      const $ = cheerio.load(response.data);
+
+      let word = $(".section #shared_section")
+        .find("#random_word")
+        .eq(0)
+        .text()
+        .replace("\r\n\t\t\t\t\t", "")
+        .replace("\r\n\t\t\t\t", "")
+        .replace("\n\t\t\t\t\t", "")
+        .replace("\n\t\t\t\t", "")
+        .replaceAll('"', "");
+      if (word.length > 15) {
+        console.log("*** Lengthy skipped");
+        return;
+      }
+
+      if (fs.existsSync(filePath)) {
+        const data = fs.readFileSync(filePath);
+        if (data.includes(word)) {
+          console.log("*** Duplicate skipped");
+          return;
+        }
+      }
+
+      fs.appendFileSync(filePath, `${word}\n`);
+      console.log(word, "- saved");
+    })
+    .catch(function (error) {
+      console.log("Something Went Wrong");
+    });
+}
